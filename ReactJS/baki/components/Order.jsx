13fb124b60cf22a6978716f441e14bdf7d83bec6
Iamnotbao <ohshit30102015@gmail.com>
2024-10-10 17:22:13 +0700

    Update Order.jsx

--- ReactJS/baki/components/Order.jsx
+++ ReactJS/baki/components/Order.jsx
@@ -1,15 +1,30 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { useSelector } from "react-redux";
+import { useLocation } from "react-router-dom";
 
 
 
 const Order = () => {
     console.log("welcome");
     const baseURL = "http://127.0.0.1:3000/api/orders/newOrder"
+    const { items } = useSelector((state) => state.cart);
+    const location = useLocation();
+    const totalPrice = location.state?.totalPrice || 0;
+    const [listProduct, setListProduct] = useState([]);
+    const [listPay, setListPay] = useState(["ATM", "Momo"]);
+    const [flag,setFlag] = useState(false);
     const [data, setData] = useState({
     })
     console.log("Test data", data);
-    
+    console.log("list is:", listProduct)
+
+
+    useEffect(() => (
+        setListProduct(items.cart.items)
+    ), [items.cart.items])
+
+
 
     // const data = {
     //     shippingInfo:
@@ -27,10 +42,7 @@ const Order = () => {
     //             "09099581051"
     //     }
     //     ,
-    //     paymentInfo: {
-    //         id: "pi_1GqIC8l6Wq90MUaG9zt7M2e5",
-    //         status: "pending",
-    //     },
+    //    ,
     //     orderItems: [{
     //         product: "66c80211701807089e09457b",
     //         quantity: "1"
@@ -52,95 +64,135 @@ const Order = () => {
                 }
             });
             console.log("Here your result", response.data);
-
         }
         catch (err) {
             console.log(err);
-
         }
-
-
     }
-    const handleAddress = (event) => {
-        const result = event.currentTarget.value;
-        const name = event.currentTarget.name;
-        console.log(name);
-        
-        setData(prev=>({...prev,
-            [name]:result
+    const handleAddress = (name, result) => {
+        console.log("your address", result);
+
+        setData(prev => ({
+            ...prev,
+            [name]: result
 
         }));
     }
-    const handlePurchase=()=>{
-        console.log("here your test data",data);
+
+    const handlePurchase = () => {
+        const method = document.getElementById("payment-method");
+        const payment = method.value;
+        setData(prev => ({
+            ...prev,
+            paymentInfo: {
+                id: "pi_1GqIC8l6Wq90MUaG9zt7M2e5",
+                status: "pending",
+            },
+            orderItems:listProduct
+        }))
+        setFlag(true);
+
+        switch (payment) {
+            case "ATM":
+                console.log("ok");
+                break;
+            case "Momo":
+                
+                    if(flag){
+                        const 
+                       url = "http://localhost:3000/api/order/payment";
+                       const response = awat
+                    }
+                    
+                break;
+
+
+            default:
+                break;
+        }
+
     }
     return (
         <>
             <section className="d-flex justify-content-center flex-md-column  align-items-center">
                 <h1>Check out</h1>
-                <form action={handlePurchase}>
+                <form>
                     <table border={1}>
                         <tr>
                             <td>
                                 <label htmlFor="address">Address</label>
-                                <input type="text" placeholder="Enter your address" id="address" name="address" onChange={(event) => handleAddress(event)} />
+                                <input type="text" placeholder="Enter your address" id="address" name="address"  required onChange={(event) => handleAddress(event.currentTarget.name, event.currentTarget.value)} />
+
+                            </td>
+
+                            <td colSpan={2}>
+                                <label htmlFor="state">State</label>
+                                <input type="text" placeholder="Enter your state" name="state" id="state" required onChange={(event) => handleAddress(event.currentTarget.name, event.currentTarget.value)} />
+
                                 <label htmlFor="address">City</label>
-                                <input type="text" placeholder="Enter your city..." id="city" />
-                            </td>
-                            <td>
-                                <label htmlFor="address">State</label>
-                                <input type="text" placeholder="Enter your state" id="state" />
-                            </td>
-                            <td>
-                                <label htmlFor="address">Country</label>
-                                <input type="text" placeholder="Enter your country" id="country" />
+                                <input type="text" placeholder="Enter your city..." name="city" id="city" required onChange={(event) => handleAddress(event.currentTarget.name, event.currentTarget.value)} />
+
+                                <label htmlFor="country">Country</label>
+                                <input type="text" placeholder="Enter your country" name="country" id="country" required onChange={(event) => handleAddress(event.currentTarget.name, event.currentTarget.value)} />
                             </td>
                         </tr>
                         <tr>
-                            <td>
-                                <label htmlFor="address">Pincode</label>
-                                <input type="text" placeholder="Enter your address" id="Pincode" />
+                            <td colSpan={3}>
+                                <label htmlFor="pincode">Pincode</label>
+                                <input type="number" placeholder="Enter your pincode" name="pincode" id="pincode"  required onChange={(event) => handleAddress(event.currentTarget.name, event.currentTarget.value)} />
                             </td>
                         </tr>
                         <tr>
-                            <td>
-                                <label htmlFor="address">Phone Number</label>
-                                <input type="text" placeholder="Enter your address" id="Phone" /></td></tr>
-                        <tr><td>
-                            <select>
-                                <option active>by card</option>
-                                <option>by online</option>
-                            </select></td></tr>
-                        <tr>
-                            <td>
-                                <label htmlFor="address">Payment Info</label>
-                                <input type="text" placeholder="Enter your address" id="Phone" /></td>
-                            <td>ok</td>
-                        </tr>
-                        <tr>
-                            <td>
-                                <label htmlFor="address">Products</label>
-                                <input type="text" placeholder="Enter your address" id="Phone" />
-                            </td>
-                            <td>
-                                <p>Item price</p>
+                            <td colSpan={3}>
+                                <label htmlFor="phone">Phone Number</label>
+                                <input type="number" placeholder="Enter your phone" name="phone" required id="phone" onChange={(event) => handleAddress(event.currentTarget.name, event.currentTarget.value)} />
                             </td>
                         </tr>
                         <tr>
-                            <td>
-                                <p>Tax price</p>
+                            <td colSpan={3}>
+                                Payment method
+                                <br />
+                                <select id="payment-method">
+                                    <option value={listPay[0]} name="atm" active>ATM</option>
+                                    <option value={listPay[1]} name="momo">Momo</option>
+                                </select></td>
+                        </tr>
+                        <tr>
+                            <td colSpan={3}>
+                                <fieldset>
+                                    <legend>
+                                        Products
+                                    </legend>
+                                    {listProduct.length !== 0 && (
+                                        listProduct.map((item, index) => (
+                                            <div key={item._id} style={{ border: "1px solid black" }}>
+                                                <ul className="d-flex align-center justify-content-between " style={{ listStyle: "none", padding: "10px" }}>
+                                                    <li>{item.productId.name}</li>
+                                                    <li>{item.productId.price}$</li>
+                                                    <li>{item.productId.description}</li>
+                                                    <li>x{item.quantity}</li>
+                                                    <li>{item.productId.rating}</li>
+                                                </ul>
+                                            </div>
+                                        )
+                                        ))}
+
+
+                                </fieldset>
                             </td>
-                            <td>
-                                <p>Ship price</p>
-                            </td>
-                            <td>
-                                <p>Total price</p>
+                        </tr>
+                        <tr>
+                            <td colSpan={3} className="text-right">
+                                <p >Tax price: 10% </p>
+                                <p>Ship price: 20$ </p>
+                                <p>Total price: {totalPrice}$</p>
                             </td>
                         </tr>
                     </table>
-                    <button onClick={handlePurchase}>Purchase</button>
-                    <button type="submit">Submit</button>
+                    
+                    <button type="submit" style={{ border: "1px solid black", backgroundColor: "grey", color: "white", opacity: "0.5", borderRadius: "5px" }}>Cancel</button>
                 </form>
+                <button onClick={handlePurchase} style={{ border: "1px solid black", backgroundColor: "grey", color: "white", opacity: "0.5", borderRadius: "5px" }}>Purchase</button>
 
             </section>
         </>
